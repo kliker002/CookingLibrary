1f0a1b19fe6eebb9d775d9e60263580a0b6b1df4
kliker02 <laker.tv@bk.ru>
2019-04-25 11:13:10 +0300

    added fragments for nav drawer activity

--- app/src/main/java/com/cooking/library/cookinglibrary/Main.java
+++ app/src/main/java/com/cooking/library/cookinglibrary/Main.java
@@ -3,6 +3,8 @@ package com.cooking.library.cookinglibrary;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private better_fragment better_fragment;
+    private better_fragment better_fragment1;
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+    private All_Recepts_Fragment all_recepts_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +40,16 @@ public class Main extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        better_fragment = new better_fragment();
+        manager = getSupportFragmentManager();
+        all_recepts_fragment = new All_Recepts_Fragment();
+
+        onNavigationItemSelected(navigationView.getMenu().findItem(R.id.nav_lib));
+
+
     }
 
     @Override
@@ -71,20 +90,63 @@ public class Main extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+//        manager.beginTransaction().remove(better_fragment).commit();
+//        manager.beginTransaction().remove(all_recepts_fragment).commit();
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_lib) {
 
-        }  else if (id == R.id.nav_share) {
+            transaction = manager.beginTransaction();
 
-        } else if (id == R.id.nav_send) {
+//            transaction.add(R.id.main_fragment, all_recepts_fragment);
+            transaction.replace(R.id.main_fragment, all_recepts_fragment);
 
+            transaction.commit();
+
+        }
+        else if (id == R.id.nav_gallery) {
+            manager.beginTransaction().remove(better_fragment).commit();
+            manager.beginTransaction().remove(all_recepts_fragment).commit();
+        }
+        else if (id == R.id.nav_better) {
+            transaction = manager.beginTransaction();
+
+//            transaction.add(R.id.main_fragment,better_fragment);
+            transaction.replace(R.id.main_fragment,better_fragment);
+
+            transaction.commit();
+        }
+        else if (id == R.id.nav_share) {
+            manager.beginTransaction().remove(better_fragment).commit();
+            manager.beginTransaction().remove(all_recepts_fragment).commit();
+        }
+
+        else if (id == R.id.nav_send) {
+            manager.beginTransaction().remove(better_fragment).commit();
+            manager.beginTransaction().remove(all_recepts_fragment).commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // buttons actions:
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.egg_btn:
+                if (findViewById(R.id.egg_recept).getVisibility() == View.GONE){
+                    findViewById(R.id.egg_recept).setVisibility(View.VISIBLE);
+                }else{
+                    findViewById(R.id.egg_recept).setVisibility(View.GONE);
+                }
+                break;
+            case R.id.meat_btn:
+                if (findViewById(R.id.meat_recept).getVisibility() == View.GONE){
+                    findViewById(R.id.meat_recept).setVisibility(View.VISIBLE);
+                }else{
+                    findViewById(R.id.meat_recept).setVisibility(View.GONE);
+                }
+                break;
+        }
     }
 }
