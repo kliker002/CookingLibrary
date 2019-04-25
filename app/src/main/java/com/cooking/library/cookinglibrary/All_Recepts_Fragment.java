297e1167d2b1cd1a7769f05d56d2f6a5371fa897
kliker02 <laker.tv@bk.ru>
2019-04-25 11:15:23 +0300

    added fragments for nav drawer activity

---
+++ app/src/main/java/com/cooking/library/cookinglibrary/All_Recepts_Fragment.java
@@ -0,0 +1,19 @@
+package com.cooking.library.cookinglibrary;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Александр on 25.04.2019.
+ */
+
+public class All_Recepts_Fragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.all_recepts_fragment,null);
+    }
+
+}
